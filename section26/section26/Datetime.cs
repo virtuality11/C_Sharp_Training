8ea66de84002015dc050d6ed0f5e936621a0b1ee
ayush <ayush.juyal@remotestate.com>
2023-12-29 17:09:13 +0530

    added generic return type

---
+++ section26/section26/Datetime.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class DateTimee
+{
+    static void Main(string[] args)
+    {
+        // Atleast three values , year , months and day have to be passed otherwise if you try to print sth , it will show runtime exception
+        DateTime dt2 = new DateTime(2001, 11 , 2 , 10 , 32 , 21 , 23);
+        Console.WriteLine(dt2.Month);
+        Console.WriteLine(dt2.Hour); 
+
+
+    }
+}
