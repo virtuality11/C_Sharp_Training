b75f39b661e7b267d5a0549ca048bc3307fa1615
ayush <ayush.juyal@remotestate.com>
2024-01-03 22:32:08 +0530

    completed the task using reflection

---
+++ ReflectionTask/ReflectionTask/Program.cs
@@ -0,0 +1,15 @@
+﻿using ReflectionTask.Models;
+using ReflectionTask.Services;
+
+namespace ReflectionTask {
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Employee e = new("Ayush", 23, "12344", 23000, "SDE", "SD1");
+            ReadFile.AddDataToFile(e);
+        }
+
+    }
+}
---
+++ ReflectionTask/ReflectionTask/Services/ReadFile.cs
@@ -0,0 +1,45 @@
+﻿using System.Reflection;
+using ReflectionTask.Models;
+
+namespace ReflectionTask.Services
+{
+    public class ReadFile
+    {
+
+        public static void AddDataToFile(Employee e)
+        {
+            string path = @"C:\Users\ayush\Desktop\htmldata.txt";
+
+            string[] data = File.ReadAllLines(path);
+
+            Type t = typeof(Employee);
+            PropertyInfo[] props = t.GetProperties();
+
+
+            for (int i = 0; i<data.Length; i++)
+            {
+                int pos = data[i].IndexOf("{{");
+                if (pos == -1) continue;
+
+                string prop = "";
+                pos += 2;
+
+                while (data[i][pos] != '}')
+                {
+                    prop += data[i][pos++];
+                }
+
+                foreach (PropertyInfo pi in props)
+                {
+                    if (pi.Name == prop)
+                    {
+                        data[i] = data[i].Replace("{{" + prop + "}}", pi.GetValue(e).ToString());
+                        break;
+                    }
+                }
+            }
+
+            File.WriteAllLines(path, data);
+        }
+    }
+}
